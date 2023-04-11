96f5ea173bc407a77a7a2984b5b1de265056542c
emilio <emilio5139@gmail.com>
2023-04-11 13:28:28 -0400

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+   
+      {
+        source: "/api",
+        destination: "https://www.3lawsrobotics.io/",
+      },
+    ];
+   },
 }
 
 module.exports = nextConfig
--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+import { httpProxyMiddleware } from 'http-proxy-middleware';
+
+export default (req, res) => httpProxyMiddleware(req, res, {
+  // You can use the `http-proxy` option
+  target:'https://emilio5139.wixsite.com/test',
+  // In addition, you can use the `pathRewrite` option provided by `next-http-proxy`
+  pathRewrite: {
+  '^/api/google':'https://google.com',
+  '^/api/myhome':'https://github.com/stegano'
+  },
+});
